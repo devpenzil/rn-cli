890b17bc1c95687d6da4e563f9ebb707d935a37e
devpenzil <ajo.a@neoito.com>
2022-10-03 14:19:14 +0530

    clone repo

---
+++ src/commands/create-project.js
@@ -0,0 +1,24 @@
+import shell from "shelljs";
+import * as fs from "fs";
+import chalk from "chalk";
+const regex = new RegExp(/^[a-zA-Z]+$/);
+export async function createProject(appname) {
+  const validationStatus = await validate(appname);
+  if (validationStatus) {
+    shell.exec(
+      `git clone https://github.com/devpenzil/clidevpenzil ${appname}`
+    );
+    shell.cd(appname);
+    shell.exec("npm install");
+    shell.cd("ios");
+    shell.exec("pod install");
+  } else {
+    console.log(chalk.red("Invalid Project name"));
+  }
+}
+function validate(appname) {
+  return new Promise((resolve, reject) => {
+    return resolve(regex.test(appname));
+  });
+}
+export default { createProject };
--- src/index.js
+++ src/index.js
@@ -1,10 +1,23 @@
 #! /usr/bin/env node
-import inquirer from "inquirer";
-const Start = () => {
-  const method = inquirer.prompt({
-    name: "TYPE",
-    type: "list",
-    choices: ["Project", "Component", "Screen"],
-  });
+import { createProject } from "./commands/create-project.js";
+import yargs from "yargs";
+import chalk from "chalk";
+const args = yargs(process.argv.slice(2)).parseSync();
+const Start = async () => {
+  if (args._.includes("generate")) {
+    if (args.project) {
+      console.log("project");
+    } else if (args.component) {
+      console.log("component");
+    } else if (args.screen) {
+      console.log("screen");
+    } else {
+      console.log("invalid flag");
+    }
+  } else if (args._.includes("create-app")) {
+    createProject(args._[1]);
+  } else {
+    console.log(chalk.red("command not found"));
+  }
 };
 Start();
