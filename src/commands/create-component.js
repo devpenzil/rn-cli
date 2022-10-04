379cfcc997d44fd56927f0e631904191a582182b
devpenzil <ajo.a@neoito.com>
2022-10-04 22:04:04 +0530

    latest

--- src/commands/create-component.js
+++ src/commands/create-component.js
@@ -1,22 +1,24 @@
 import { validator } from "../utils/Validator.js";
 import chalk from "chalk";
 import shell from "shelljs";
-import ejs from "ejs";
 import * as fs from "fs";
-
+import * as ejs from "ejs";
 const componentTemplate = fs.readFileSync(
   "/Users/ajoalex/Documents/projects/rn-cli/src/template/component.ejs",
   "utf-8"
 );
-const some = ejs.render(componentTemplate, { user: "ajo" });
-console.log(some);
 export async function createComponent(appname) {
+  const some = ejs.render(componentTemplate, { component: appname });
   const validationStatus = await validator(appname);
   if (validationStatus) {
     shell.mkdir(appname);
     shell.cd(appname);
-    shell.exec(`echo 'hello' > ${appname}.component.tsx`);
-    // shell.exec(`echo ${style} > ${appname}.style.tsx`);
+    shell.touch(`${appname}.component.tsx`);
+    shell.touch(`${appname}.style.ts`);
+    fs.writeFileSync(
+      `/Users/ajoalex/Documents/projects/rn-cli/${appname}/${appname}.component.tsx`,
+      some
+    );
   } else {
     console.log(chalk.bgRed("Invalid name format"));
   }
